82b912d54d616bd0dce77c1b19dd8152910b7d42
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-03-18 19:39:02 -0300

    ✨ add Header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<header className="w-full py-6 px-4 fixed bg-white top-0 z-10">
+			<div className="max-w-7xl mx-auto flex items-center justify-between">
+				<div className="flex items-center space-x-2">
+					<h1 className="text-xl text-black font-medium tracking-tight">TextAlchemy</h1>
+				</div>
+				<div className="flex items-center space-x-1">
+					<span className="text-xs text-[#2463eb] px-2 py-1 rounded-full bg-[#e9effc] font-medium">
+						AI-Powered
+					</span>
+				</div>
+			</div>
+		</header>
+	);
+}
